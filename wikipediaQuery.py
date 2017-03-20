4ad1e4705f4d3a1e329d0a42c0a04873182e0e48
David Mwangi Mathenge <david.mathenge98@gmail.com>
2017-03-20 07:51:21 +0300

    Added language preference. User can choose to use one of the 5 languages(English, Spanish, French, Chinese or German

--- wikipediaQuery.py
+++ wikipediaQuery.py
@@ -1,7 +1,15 @@
 import wikipedia
 
+print ("What is your preferred language? Below are some choices: ")
+
+print ("1. Type en for English\n2. Type es for Spanish\n3. Type fr for French\n4. Type zh for Chinese\n5. Type de for German")
+
+lang = str(input("Enter choice: "))
+
 while True:
     
-    user_input = input ("Ask  me a question: ")
-
+    user_input = input ("\nAsk  me a question: ")
+    
+    wikipedia.set_lang(lang)
+    
     print (wikipedia.summary(user_input))
